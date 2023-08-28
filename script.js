3507be3bb9264232975726e774c1b8e0d9d8e5a8
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2023-08-28 18:18:14 -0300

    Finalização da função para limpar os inputs

--- script.js
+++ script.js
@@ -4,4 +4,19 @@ function addPlayer() {
   const number = document.getElementById("number").value;
 
   console.log({ position, name, number });
+  const confirmar = confirm(
+    "Escalar " + name + " na posição " + position + "?"
+  );
+  if (confirmar) {
+    const teamlist = document.getElementById("team-list");
+    const player = document.createElement("li");
+    player.id = "player" + number;
+    player.innerText = position + ": " + name + " (" + number + " )";
+    teamlist.appendChild(player);
+
+    // Limpar os inputs
+    document.getElementById("position").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("number").value = "";
+  }
 }
