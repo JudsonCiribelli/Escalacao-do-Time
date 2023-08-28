e9b040b78d6b6aef2d3bfbf24729ff4390fc101a
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2023-08-28 18:57:03 -0300

    Finalização do programa

--- script.js
+++ script.js
@@ -3,20 +3,30 @@ function addPlayer() {
   const name = document.getElementById("name").value;
   const number = document.getElementById("number").value;
 
-  console.log({ position, name, number });
-  const confirmar = confirm(
-    "Escalar " + name + " na posição " + position + "?"
-  );
-  if (confirmar) {
-    const teamlist = document.getElementById("team-list");
-    const player = document.createElement("li");
-    player.id = "player" + number;
-    player.innerText = position + ": " + name + " (" + number + " )";
-    teamlist.appendChild(player);
+  const confirmation = confirm("Escalar " + name + " como " + position + "?");
 
-    // Limpar os inputs
+  if (confirmation) {
+    const teamList = document.getElementById("team-list");
+    const playerItem = document.createElement("li");
+    playerItem.id = "player-" + number;
+    playerItem.innerText = position + ": " + name + " (" + number + ")";
+    teamList.appendChild(playerItem);
+
     document.getElementById("position").value = "";
     document.getElementById("name").value = "";
     document.getElementById("number").value = "";
+  }
+}
+function removePlayer() {
+  const number = document.getElementById("numberToRemove").value;
+  const playerToRemove = document.getElementById("player-" + number);
+
+  const confirmation = confirm(
+    "Remover o jogador " + playerToRemove.innerText + "?"
+  );
+
+  if (confirmation) {
+    document.getElementById("team-list").removeChild(playerToRemove);
+    document.getElementById("numberToRemove").value = "";
   }
 }
